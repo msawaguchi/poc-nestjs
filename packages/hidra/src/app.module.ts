c354286236aa50b46cb92ba59bf455ae2cab70a3
Brenda <brendaprofiro@gmail.com>
2021-03-24 17:42:58 -0300

    chore: Use new controller

--- packages/hidra/src/app.module.ts
+++ packages/hidra/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './purchases.controller';
 import { AppService } from './app.service';
 
 @Module({
